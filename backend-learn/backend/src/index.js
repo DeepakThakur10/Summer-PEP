2b370ce1b6b1e0e4e03d323d7a028a577d460934
Deepak Kumar Thakur <dk1816859@gmail.com>
2026-07-21 09:06:13 +0530

    feat: add login endpoint with JWT authentication and user validation

--- backend-learn/backend/src/index.js
+++ backend-learn/backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import fs from "fs/promises";
 import "dotenv/config";
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 
@@ -75,9 +76,60 @@ app.post("/signup", async (req, res) => {
         message: "Internal Server Error"
     });
 }
-
    
 });
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({
+            message: "Email and password are required to login"
+        });
+    }
+
+    const users = JSON.parse(
+        await fs.readFile("data/user.json", "utf-8")
+    );
+
+    const user = users.find(u => u.email === email);
+
+    if (!user) {
+        return res.status(404).json({
+            message: "User not found"
+        });
+    }
+
+    const isMatched = await bcrypt.compare(password, user.password);
+
+    if (!isMatched) {
+        return res.status(401).json({
+            message: "Invalid credentials"
+        });
+    }
+    const token = jwt.sign(
+        {
+        id: user.id,
+        email: user.email,
+        role: user.role
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "7d"
+        }
+    )
+
+    res.status(200).json({
+        success: true,
+        message: "User logged in successfully",
+        token: token
+
+    });
+});
+
+app.get('/courses',(req,res)=>{
+    
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
